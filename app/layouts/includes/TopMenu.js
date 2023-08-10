46fdc515a47b7774e97ace86c019ec6f13678778
jeffmlb91 <jeffmlb91@gmail.com>
2023-08-10 01:20:50 -0400

    add: header left menue and added right shopping cart with it static state

--- app/layouts/includes/TopMenu.js
+++ app/layouts/includes/TopMenu.js
@@ -2,10 +2,12 @@
 
 import Link from "next/link"
 import { BsChevronDown } from "react-icons/bs"
+import { AiOutlineShoppingCart } from "react-icons/ai"
 
 export default function TopMenu() {
   return (
     <>
+      {/**Building Menue for orders and Sign out Left Side */}
       <div id="TopMenu" className="border-b">
         <div className="flex items-center justify-between w-full max-auto max-w-[1200px]">
           <ul
@@ -17,7 +19,50 @@ export default function TopMenu() {
                 <div>Login</div>
                 <BsChevronDown />
               </Link>
+
+              <div id="AuthDropdown"className="hidden absolute bg-white w-[200px] text-[#333333] z-40 top-[20px] left-0 border shadow-lg">
+                <div className="flex items-center justify-start gap-1 p-3">
+                    <img width={50} src="https://picsum.photos/200"/>
+                    <div className="font-bold text-[13px]">John Weeks</div>
+                </div>
+
+                <div className="border-b"/>
+
+                <ul className="bg-white">
+                    <li className="text-[11px] py-2 px-4 w-full hover:underline text-blue-500 hover:text-blue-600 cursor-pointer">
+                        <Link href="/orders">
+                            My Orders
+                        </Link>
+                    </li>
+                    <li className="text-[11px] py-2 px-4 w-full hover:underline text-blue-500 hover:text-blue-600 cursor-pointer">
+                        Sign Out
+                    </li>
+                </ul>
+              </div>
             </li>
+            <li className="px-3 hover:underline cursor-pointer">
+                Daily Deals
+            </li>
+            <li className="px-3 hover:underline cursor-pointer">
+                Help & Contact
+            </li>
+          </ul>
+
+          {/**Defining the TopMenuRight side */}
+          <ul id="TopMenuRight" className="flex items-center text-[11px] text-[#333333] px-2 h-8">
+                <li className="flex items-center gap-2 px-3 hover:underline cursor-pointer">
+                    <img width={32} src="/images/uk.png"/> 
+                    Ship to
+                </li>
+                {/**defining shopping cart and its state */}
+                <li className="px-3 hover:underline cursor-pointer">
+                    <div className="relative">
+                        <AiOutlineShoppingCart size={22}/>
+                        <div className="absolute text-[10px] -top-[2px] -right-[5px] bg-red-500 w-[14px] h-[14px] rounded-full text-white">
+                            <div className="flex items-center justify-center -mt-[1px]">3</div>
+                        </div>
+                    </div>
+                </li>
           </ul>
         </div>
       </div>
