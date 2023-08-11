bcbad6ef5be86f81612037f19bf9ef0f2be2aacd
jeffmlb91 <jeffmlb91@gmail.com>
2023-08-10 22:17:22 -0400

    add: Main Header with the search bar and search button + the advance link button

--- app/layouts/MainLayout.js
+++ app/layouts/MainLayout.js
@@ -1,15 +1,17 @@
-"use client"
+"use client";
 
-import TopMenu from "./includes/TopMenu"
+import MainHeader from "./includes/MainHeader";
+import TopMenu from "./includes/TopMenu";
 
 export default function MainLayout({ children }) {
-    return(
-        <>
-         <div id="MainLayout" className="min-w-[1050px] max-w-[1300px] mx-auto">
-            <div>
-                <TopMenu />
-            </div>
-         </div>
-        </>
-    )
+  return (
+    <>
+      <div id="MainLayout" className="min-w-[1050px] max-w-[1300px] mx-auto">
+        <div>
+          <TopMenu />
+          <MainHeader />
+        </div>
+      </div>
+    </>
+  );
 }
---
+++ app/layouts/includes/SubMenu.js
@@ -0,0 +1,18 @@
+"use client";
+
+import Link from "next/link"
+import { BsChevronDown } from "react-icons/bs"
+import { AiOutlineShoppingCart } from "react-icons/ai"
+
+export default function TopMenu() {
+  return (
+    <>
+      {/**Building Menue for orders and Sign out Left Side */}
+      <div id="TopMenu" className="border-b">
+        <div className="flex items-center justify-between w-full max-auto max-w-[1200px]">
+         
+        </div>
+      </div>
+    </>
+  );
+}
