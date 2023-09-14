4c859fcd3559aba68138625ab86a5c3332043b48
Geoffrey Moluba <geoffreymoluba@Geoffreys-MacBook-Pro.local>
2023-09-13 22:29:23 -0400

    complete product page with product id and description. added Bitloader

---
+++ app/components/SimilarProducts.js
@@ -0,0 +1,50 @@
+"use client";
+
+import { BiLoader } from "react-icons/bi";
+import ProductComp from "./Product";
+
+
+export default function SimilarProducts() {
+
+    const products = [
+        {
+          id: 1,
+          title: "Brown Leather Bag",
+          description:
+            "This Brown Leather Bag is so fashionable that it is a head turner. Purchase it today and enjoy the best quality It's essentially a mock-up of the final book, complete with all the text and visuals in their correct positions.",
+          url: "https://picsum.photos/id/7",
+          price: 2500,
+        },
+        {
+          id: 2,
+          title: "School Books",
+          description:
+            "Enoy this school book to learn about web programing in depth. From Front-End to Back-End. Enjoy learning FullStack developement. A Web Developer is a professional who is responsible for the design and construction of websites. They ensure that sites meet user expectations",
+          url: "https://picsum.photos/id/20",
+          price: 1999,
+        },
+    ];
+
+  return (
+    <>
+      <div>
+        <div className="border-b py-1 max-w-[1200px] mx-auto"/>
+
+        <div className="max-w-[1200px] mx-auto">
+            
+            <div className="fomt-bold text-2xl py-2 mt-4">
+                Similar sponsored items
+            </div>
+
+            {products.length > 0 ? <div className="grid grid-cols-5 gap-4">{products.map(product => (<ProductComp key={products.id} product={product} />))}</div>
+            : <div className="flex items-center justify-center">
+                <div className="flex items-center justify-center gap-4 font-semibold">
+                    <BiLoader size={30} className="text-blue-400 animated-spin"/>
+                    Loading Products...
+                </div>
+              </div>}
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ app/product/[id]/page.js
@@ -0,0 +1,70 @@
+"use client"
+
+import MainLayout from "@/app/layouts/MainLayout"
+import SimilarProducts from "../../components/SimilarProducts"
+
+export default function Product({ params }) {
+
+    const product = {
+          id: 1,
+          title: "Brown Leather Bag",
+          description:
+            "This Brown Leather Bag is so fashionable that it is a head turner. Purchase it today and enjoy the best quality It's essentially a mock-up of the final book, complete with all the text and visuals in their correct positions.",
+          url: "https://picsum.photos/id/7",
+          price: 2500,
+    }
+
+    return (
+        <>
+            <MainLayout>
+                <div className="max-w-[1200px] mx-auto">
+                    <div className="flex px-4 py-10">
+                        {product?.url 
+                           ? <img className="w-[40%] rounded-lg" src={product?.url+'/280'}/> 
+                           : <div className="w-[40%]"></div>
+                        }
+
+                        <div className="px-4 w-full">
+                            <div className="font-bold text-xl">{product?.title}</div>
+                            <div className="text-sm text-gray-700 pt-2">Brand New - Full Warranty</div>
+
+                            <div className="border-b py-1"/>
+
+                            <div className="pt-3 pb-2">
+                                <div className="flex items-center">
+                                    Condition: <span className="font=bold text-[17px] ml-2">New</span>
+                                </div>
+                            </div>
+
+                            <div className="border-b py-1"/>
+                            
+                            <div className="pt-3">
+                                <div className="w-full flex items-center justify-between">
+                                    <div className="flex items-center">
+                                        Price:
+                                        {product?.price 
+                                         ? <div className="font-bold text-[20px] ml-2"> USD ${(product?.price / 100).toFixed(2)}</div> 
+                                         : null 
+                                        }
+                                    </div>
+
+                                    <button className="bg-[#3498c9] text-white py-2 px-20 rounded-full cursor-pointer">
+                                        Add To Cart
+                                    </button>
+                                </div>
+                            </div>
+                            <div className="border-b py-1"/>
+
+                            <div className="pt-3">
+                                <div className="font-semibold pb-1"> Description</div>
+                                <div className="text-sm">{product?.description}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <SimilarProducts />
+            </MainLayout>
+        </>
+    )
+}
