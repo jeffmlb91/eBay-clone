e0e983703addcebf6839e077c81e3ac8388a84fb
jeffmlb91 <jeffmlb91@gmail.com>
2023-09-15 23:42:36 -0400

    added checkout page completed

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -41,6 +41,45 @@ export default function Checkout() {
                                 <CheckoutItem  key={product.id} product={product}/>
                             </div>
                         </div>
+
+                        <div id="PlaceOrder" className="relative -top-[60px] w-[35%] border rounded-lg">
+                            <div className="p-4">
+                                <div className="flex items-baseline justify-between text-sm mb-1">
+                                    <div>Items  (3)</div>
+                                    <div>$12.99</div>
+                                </div>
+                                <div className="flex items-center justify-between mb-4 text-sm">
+                                  <div>shipping</div>
+                                  <div>Free</div>
+                                </div>
+
+                                <div className="border-t"/>
+
+                                <div className="flex items-center justify-between my-4">
+                                    <div className="font-semibold">Order total</div>
+                                    <div className="text-2x font-semibold">
+                                        $12.99
+                                    </div>
+                                </div>
+
+                                <form>
+                                    <div id="card-element" className="border border-gray-500 p-2 rounded-sm">
+                                        <p  id="card-error"
+                                            role="alert"
+                                            className="text-red-700 text-center font-semibold relative top-2"
+                                        />
+                                    </div>
+                                    <button type="submit" className="mt-4 bg-blue-600 text-lg w-full text-white font-semibold p-3 rounded-full">
+                                            Confirm and pay
+                                    </button>
+                                </form>
+                            </div>
+
+                            <div className="flex items-center p-4 justify-center gap-2 border-2">
+                                <img width={50} src="/images/logo.svg" />
+                                <div className="font-light mb-2 mt-2">MONEY BACK GUARANTEE</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </MainLayout>
