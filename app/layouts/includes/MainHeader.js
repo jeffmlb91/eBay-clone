cc78ab6a30258732b28dd819888aaee120f8db3b
jeffmlb91 <jeffmlb91@gmail.com>
2023-09-24 03:16:43 -0400

    added search functionality in the header search bar. Created new folder name in search-by name for enabaling search functionality

--- app/layouts/includes/MainHeader.js
+++ app/layouts/includes/MainHeader.js
@@ -1,10 +1,43 @@
 "use client";
 
+import { debounce } from "debounce";
 import Link from "next/link";
+import { useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { BiLoaderCircle } from "react-icons/bi";
 
-export default function MainHeader() {
+export default function MainHeader() { 
+
+  const [ items, setItems ] = useState([])
+  const [ isSearching, setIsSearching ] = useState(null)
+
+  const handleSearchName = debounce(async (event) => {
+    if (event.target.value == "") {
+      setItems([])
+      return
+    }
+
+    setIsSearching(true)
+
+    try {
+        const response = await fetch(`/api/products/search-by-name/${event.target.value}`)
+        const result = await response.json()
+
+        if (result) {
+          setItems(result)
+          setIsSearching(false)
+          return
+        }
+
+        setItems([])
+        setIsSearching(false)
+    } catch (error) {
+        console.log(error)
+        alert(error)
+    }
+    
+  }, 500)
+
   return (
     <>
       {/**Building Main Header with Search Bar and Search button plus advance link*/}
@@ -25,9 +58,32 @@ export default function MainHeader() {
 
                       <input
                         className="w-full placeholder-gray-400 text-sm pl-3 focus:outline-none"
+                        onChange={handleSearchName}
                         placeholder="Search for anything"
                         type="text"
                       />
+
+                      {isSearching ? <BiLoaderCircle className="mr-2 animate-spin" size={22}/> : null}
+
+                      {items.length > 0 ?
+                          <div className="absolute bg-white max-w-[910px] h-auto w-full z-20 left-0 top-12 border p-1">
+                            {items.map((item) => (
+                              <div className="p-1" key={item.id}>
+                                <Link
+                                  href={`/product/${item?.id}`}
+                                  className="flex items-center justify-between w-full cursor-pointer hover:bg-gray-200 p-1 px-2"
+                                >
+                                    <div className="flex items-center">
+                                      <img className="rounded-md" width="40" src={item?.url+'/40'}/>
+                                      <div className="truncate ml-2">{item?.title}</div>
+                                    </div>
+                                    <div>${ (item?.price / 100).toFixed(2) }</div>
+                                </Link>
+                              </div>
+                            )) }
+                          </div>
+                      : null}
+
                     </div>
                     {/**Search Button */}
                     <button className="flex items-center bg-blue-600 text-sm font-semibold text-white p-[11px] ml-2 px-14">
